413df706790f89392dc87e376d38dc6ef2ae47d1
Max <ipust13@gmail.com>
2019-06-25 21:19:30 +0300

    Foreground tracking

--- logger.js
+++ logger.js
@@ -1,25 +1,46 @@
 var timer;
+var username = "";
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('start');
+    // onClick's logic below:
+    link.addEventListener('click', start);
+});
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('stop');
+    // onClick's logic below:
+    link.addEventListener('click', stop);
+});
+
+function changeButtonState(inProgress) {
+    inProgress = inProgress || false;
+    document.getElementById("start").disabled = inProgress;
+    document.getElementById("stop").disabled = !inProgress;
+}
 
 function start() {
-    timer = setTimeout()
+    username = document.getElementById("username").value;
+    if (username === "") {
+        alert("Enter username!");
+        return
+    }
+    changeButtonState(true);
+    timer = setInterval(sendCurrentUrl, 1000)
 }
 
 function stop() {
-    clearTimeout(timer)
+    clearTimeout(timer);
+    changeButtonState(false);
 }
 
-function loigShit() {
-    console.log("shome shot")
-
-}
-
-function sendShit() {
+function sendRecord(pageUrl) {
     fetch("https://tsdb.informatik.uni-rostock.de:8086/write?db=loggerTestDB", {
         method: "POST",
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
-        body: "activity_tracking,user=max file=\"test ext\",process=\"some process\",app=\"Google Chrome Ext\""
+        body: "activity_tracking,user=max file=\""+ pageUrl +"\",app=\"Google Chrome Ext\""
     }).then(function (response) {
         console.log(response);
         return response.json();
@@ -27,4 +48,11 @@ function sendShit() {
         .then(function (myJson) {
             console.log(JSON.stringify(myJson));
         });
+}
+
+function sendCurrentUrl() {
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = tabs[0].url;
+        sendRecord(url)
+    });
 }
