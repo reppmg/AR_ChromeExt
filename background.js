f17a1e378bdb061fef570d5e2507cf74609166cf
chernyshevaa <chernysheva.anastasiia@gmail.com>
2019-07-04 00:30:50 +0300

    move send request logic to background

--- background.js
+++ background.js
@@ -1,1 +1,41 @@
-alert("lel")
+var timer;
+
+chrome.runtime.onMessage.addListener(function (message, callback) {
+    console.log("received message")
+    if (message.type === "start") {
+        alert("Hello, " + message.username + "!");
+
+        timer = setInterval(sendCurrentUrl(message.username), 1000)
+    }
+    ;
+});
+
+chrome.runtime.onMessage.addListener(function (message, callback) {
+    if (message.type === "stop") {
+        clearTimeout(timer);
+        alert("STOP IT NOW!!!!")
+    }
+});
+
+function sendRecord(username, pageUrl) {
+    fetch("https://tsdb.informatik.uni-rostock.de:8086/write?db=loggerTestDB", {
+        method: "POST",
+        headers: {},
+        body: "activity_tracking,user=" + username + " file=\"" + pageUrl + "\",app=\"Google Chrome Ext\""
+    }).then(function (response) {
+
+        console.log(response);
+        return response.json();
+    })
+        .then(function (myJson) {
+            console.log(JSON.stringify(myJson));
+        });
+}
+
+function sendCurrentUrl(username) {
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = tabs[0].url;
+        // alert(url)
+        sendRecord(username, url)
+    });
+}
--- logger.js
+++ logger.js
@@ -1,13 +1,13 @@
-var timer;
+
 var username = "";
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var link = document.getElementById('start');
     // onClick's logic below:
     link.addEventListener('click', start);
 });
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var link = document.getElementById('stop');
     // onClick's logic below:
     link.addEventListener('click', stop);
@@ -22,37 +22,22 @@ function changeButtonState(inProgress) {
 function start() {
     username = document.getElementById("username").value;
     if (username === "") {
-        alert("Enter username!");
+        document.getElementById("ar_ext_label").innerHTML = "Please, insert username!";
         return
     }
+
+    chrome.runtime.sendMessage({type: "start", username: username}, function (response) {
+        console.log("name sent !!!");
+    });
+
     changeButtonState(true);
-    timer = setInterval(sendCurrentUrl, 1000)
 }
 
 function stop() {
-    clearTimeout(timer);
+    chrome.runtime.sendMessage({type: "stop"}, function (response) {
+
+    });
+
     changeButtonState(false);
 }
 
-function sendRecord(pageUrl) {
-    fetch("https://tsdb.informatik.uni-rostock.de:8086/write?db=loggerTestDB", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: "activity_tracking,user=max file=\""+ pageUrl +"\",app=\"Google Chrome Ext\""
-    }).then(function (response) {
-        console.log(response);
-        return response.json();
-    })
-        .then(function (myJson) {
-            console.log(JSON.stringify(myJson));
-        });
-}
-
-function sendCurrentUrl() {
-    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-        var url = tabs[0].url;
-        sendRecord(url)
-    });
-}
